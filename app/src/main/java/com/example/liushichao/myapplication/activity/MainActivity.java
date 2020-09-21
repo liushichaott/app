ef425ca5b408a852dc2fe2e377b327fa228c08b2
liushichao <15639257496@163.com>
2020-09-21 23:04:27 +0800

    2

--- app/src/main/java/com/example/liushichao/myapplication/activity/MainActivity.java
+++ app/src/main/java/com/example/liushichao/myapplication/activity/MainActivity.java
@@ -21,7 +21,7 @@ public class MainActivity extends Activity implements View.OnClickListener {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main);//2
         startFloatWindow = findViewById(R.id.start_float_window);
         createClass = findViewById(R.id.createClass);
         lookClass = findViewById(R.id.lookClass);
